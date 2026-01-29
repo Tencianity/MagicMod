91ac08c6292274fe67e7a001512b0616816482ca
Tencianity <arendechabert@gmail.com>
2026-01-29 17:07:09 -0500

    Testing

---
+++ src/client/java/github/tencianity/MagicModClient.java
@@ -0,0 +1,10 @@
+package github.tencianity;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/github/tencianity/MagicMod.java
@@ -0,0 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package github.tencianity;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MagicMod implements ModInitializer {
+	public static final String MOD_ID = "magicmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger("magicmod");
+
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
